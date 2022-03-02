5bd8c75fa72d2bb7002e595018e12492e95fecb5
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-02 10:23:56 +0000

    "started with the footer html"

---
+++ src/components/footer/footer.js
@@ -0,0 +1,32 @@
+
+const Footer = () => {
+  return (
+    <div>
+       <div>
+        <img src="" alt=""/>
+       </div>
+
+          <div>
+            <h3>Features</h3>
+            <span>Link Shortening</span>
+            <span> Branded Links</span>
+            <span>Analytics</span>
+          </div>
+          <div>
+            <h3> Resources</h3>
+            <span>Blog</span>
+            <span>Developers</span>
+            <span>Support</span>
+          </div>
+          <div>
+            <h3> Company</h3>
+            <span>About</span>
+            <span>Our Team</span>
+            <span>Careers</span>
+            <span>Contact</span>
+          </div>
+    </div>
+  )
+}
+
+export default Footer
