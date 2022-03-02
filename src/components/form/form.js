fb3b39f404d990f3bf4bda0f504b1956c17ad0fe
unknown <david.aboagye@amalitech.org>
2022-03-02 16:17:03 +0000

    added background image

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -3,12 +3,14 @@ import "./form.css";
 
 const Form = () => {
   return (
+      <div id="formContainer">
     <div id="formWrap">
             <form>
         <div id="formControl">
           <input type="text" id="url" placeholder="Shorten a link here..." />
         </div>
       </form>
+    </div>
     </div>
   );
 };
