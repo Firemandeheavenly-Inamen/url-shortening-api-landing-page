9aa75ab65ae21114c18413f1d58319ae3e785872
unknown <david.aboagye@amalitech.org>
2022-03-03 16:24:30 +0000

    added a label

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -5,6 +5,7 @@ const Form = () => {
   return (
       <div id="formContainer">
     <div id="formWrap">
+      <label id="hiddenLabel">Shorten a link here</label>
             <form>
         <div id="formControl">
           <input type="text" id="url" placeholder="Shorten a link here..." />
