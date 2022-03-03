1f7fd121799edc85801ba2fba0f4444ec3f066fd
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-03 23:36:01 +0000

    "insert the social icons for the footer"

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,10 @@
-import whitelogo from ".././images/whitelogo.svg";
-import footer from "../footer/footer.css"
+import whitelogo from "../images/whitelogo.svg";
+import footer from "../footer/footer.css";
+import facebook from "../images/icon-facebook.svg";
+import twitter  from "../images/icon-twitter.svg";
+import printerest from "../images/icon-pinterest.svg";
+import instagram from "../images/icon-instagram.svg";
+
 
 const Footer = () => {
   return (
@@ -11,7 +16,9 @@ const Footer = () => {
             <div className="list">
                 <div className="list-title">
                     <ul>
-                        <h3>Features</h3>
+                        <div>
+                           <h3>Features</h3> 
+                        </div>
                         <li>Link Shortening</li>
                         <li>Branded Links</li>
                         <li>Analytics</li>
@@ -19,7 +26,9 @@ const Footer = () => {
                 </div>
                 <div className="list-title">
                     <ul>
-                        <h3> Resources</h3>
+                        <div>
+                            <h3> Resources</h3>
+                        </div>
                         <li>Blog</li>
                         <li>Developers</li>
                         <li>Support</li>
@@ -27,7 +36,9 @@ const Footer = () => {
                 </div>
                 <div className="list-title">
                     <ul>
-                        <h3> Company</h3>
+                        <div>
+                            <h3> Company</h3>
+                        </div>
                         <li>About</li>
                         <li>Our Team</li>
                         <li>Careers</li>
@@ -37,9 +48,10 @@ const Footer = () => {
             </div>
             <div className="media">
                 <ul>
-                    <li>Fb</li>
-                    <li>Twitter</li>
-                    <li>Insta</li>
+                    <li><img src={facebook} alt="facebook"/></li>
+                    <li><img src={twitter} alt="twitter" /></li>
+                    <li><img src={printerest} alt="printerest" /></li>
+                    <li><img src={instagram} alt="Instagram" /></li>
                 </ul>
             </div>
         </div>
