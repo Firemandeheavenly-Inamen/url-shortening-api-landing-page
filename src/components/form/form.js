97954714e3f0017595aa9e5d6c0410e91e000c05
unknown <david.aboagye@amalitech.org>
2022-03-04 01:56:20 +0000

    removed cretain things

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -3,21 +3,14 @@ import "./form.css";
 
 const Form = () => {
   return (
-    <section id='shortenContainer'>
-    <div id='shortenWrap'>
-      <div id="formContainer">
     <div id="formWrap">
             <form>
         <div id="formControl">
-        <label id="hiddenLabel" htmlFor='url'>Shorten a link here</label>
           <input type="text" id="url" placeholder="Shorten a link here..." />
           <button id="submitButton">Shorten It!</button>
         </div>
       </form>
     </div>
-    </div>
-    </div>
-    </section>
   );
 };
 
