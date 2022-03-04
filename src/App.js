eaa357feb19886c79cd8b4c6cddce8f37a008747
Reuben-Ekow-Yawson <reuben.yawson@amalitech.org>
2022-03-04 12:02:52 +0000

    "imported the getStarted.js"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Footer from './components/footer/footer'
+import GetStarted from './components/Get-Started/getStarted';
 
 function App() {
   return (
     <div className="App">
+      <GetStarted />
       <Footer />
     </div>
   );
