86d843cb73d9db3b629a34aa5e8f0bac3491dd08
unknown <david.aboagye@amalitech.org>
2022-03-05 08:02:34 +0000

    imports made

---
+++ src/components/form/ShortlyAppElements.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import shortenMobile from '../../images/bg-shorten-mobile.svg';
+import shortenDesktop from '../../images/bg-shorten-desktop.svg';
+import { Form, FormControl } from './Forms';
