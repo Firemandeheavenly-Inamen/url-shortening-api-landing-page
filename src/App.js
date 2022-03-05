4d6c149c10523028f49338cfd56b5941caede523
unknown <david.aboagye@amalitech.org>
2022-03-05 07:10:46 +0000

    resolving app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-<<<<<<< HEAD
 import Navbar from './components/navbar/navbar';
 import Hero from './components/hero/hero';
-=======
-import ShortlyApp from './components/form/ShortlyApp'
->>>>>>> form
-
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Navbar />
       <Hero />
-=======
-      <ShortlyApp/>
->>>>>>> form
     </div>
   );
 }
