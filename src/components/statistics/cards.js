6879bc3b2f0e363f961b1c07899cf927ae6a6a24
unknown <david.aboagye@amalitech.org>
2022-03-07 09:00:03 +0000

    card one

---
+++ src/components/statistics/cards.js
@@ -0,0 +1,6 @@
+export const CardOne = {
+    img: require('../../images/icon-brand-recognition.svg').default,
+    heading: 'Brand Recognition',
+    copy: "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
+};
+
