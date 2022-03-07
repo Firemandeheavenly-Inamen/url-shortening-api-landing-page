df8e664980538ad95295163c3d4a74646369cbb9
unknown <david.aboagye@amalitech.org>
2022-03-07 10:50:07 +0000

    statistics section elements

--- src/components/statistics/StatsSectionElements.js
+++ src/components/statistics/StatsSectionElements.js
@@ -27,3 +27,110 @@ export const StatsCopy = styled.p`
     }
 `;
 
+export const StatsCardsContainer = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 5.75rem;
+
+    &::before {
+        content: '';
+        position: absolute;
+        width: 0.5rem;
+        height: 100%;
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: var(--primary);
+        
+    }
+
+    @media screen and (min-width: 1024px) {
+        flex-direction: row;
+        gap: 1.75rem;
+        height: 22.25rem;
+
+        &::before {
+            width: 100%;
+            height: 0.5rem;
+            left: unset;
+            top: 40%;
+            transform: translate(0);
+        }
+    }
+`;
+
+export const StatsCardWrap = styled.div`
+    position: relative;
+    padding: 4.75rem 2rem 2.5rem;
+    background-color: var(--background-light);
+    border-radius: 5px;
+
+    @media screen and (min-width: 1024px) {
+        height: max-content;
+        text-align: left;
+
+        &:nth-of-type(2) {
+            top: 44px;
+        }
+
+        &:nth-of-type(3) {
+            top: 88px;
+        }
+    };
+
+`;
+
+export const StatsIconWrap = styled.div`
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 5.5rem;
+    width: 5.5rem;
+    border-radius: 50%;
+    background-color: var(--secondary);
+
+    @media screen and (min-width: 1024px) {
+        left: 2rem;
+        transform: translateY(-50%);
+    };
+`;
+
+export const StatsIcon = styled.img`
+
+`;
+
+export const StatsCardHeading = styled.h3`
+    margin-bottom: 0.75rem;
+    font-size: 1.375rem;
+    line-height: 2rem;
+    color: var(--text-dark);
+`;
+
+export const StatsCardCopy = styled.p`
+    font-size: 0.9375rem;
+    line-height: 1.625rem;
+`;
+
+export const StatsSectionContainer = styled.section`
+    padding: 0 1.5rem 5rem;
+    background-color: var(--background-med);
+
+    @media screen and (min-width: 768px) {
+        padding: 0 2.5rem 5rem;
+    }
+
+    @media screen and (min-width: 1024px) {
+        padding: 0 2.5rem 7.5rem;
+    }
+`;
+
+export const StatsSectionWrapper = styled.div`
+    max-width: 1100px;
+    margin: auto;
+    text-align: center;
+`;
