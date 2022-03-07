3cd9f311c5e192e260d1c7137922526d33b6a99d
unknown <david.aboagye@amalitech.org>
2022-03-07 09:00:27 +0000

    card two

--- src/components/statistics/cards.js
+++ src/components/statistics/cards.js
@@ -4,3 +4,9 @@ export const CardOne = {
     copy: "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
 };
 
+export const CardTwo = {
+    img: require('../../images/icon-detailed-records.svg').default,
+    heading: "Detailed Records",
+    copy: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
+};
+
