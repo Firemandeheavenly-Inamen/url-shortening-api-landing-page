5a51f1b746315823e107108f9faa63b96891a90c
unknown <david.aboagye@amalitech.org>
2022-03-07 09:01:43 +0000

    card three

--- src/components/statistics/cards.js
+++ src/components/statistics/cards.js
@@ -10,3 +10,8 @@ export const CardTwo = {
     copy: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
 };
 
+export const CardThree = {
+    img: require('../../images/icon-fully-customizable.svg').default,
+    heading: "Fully Customizable",
+    copy: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+};
