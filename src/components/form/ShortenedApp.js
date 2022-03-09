6ceb8717e4f4b0c9d924d6fd3af5534e3dbae17e
unknown <david.aboagye@amalitech.org>
2022-03-09 20:18:05 +0000

    done

--- src/components/form/ShortenedApp.js
+++ src/components/form/ShortenedApp.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import useUrls from './hooks/useUrls';
-import ShortenedFormMaster from './ShortenedFormMaster';
-import LinkList from './LinkList';
-import Loader from './Loader';
-import { ErrorMessage } from './ErrorMessage';
+import React from "react";
+import useUrls from "./hooks/useUrls";
+import ShortenedFormMaster from "./ShortenedFormMaster";
+import LinkList from "./LinkList";
+import Loader from "./Loader";
+import { ErrorMessage } from "./ErrorMessage";
 import {
-    ShortenedAppContainer,
-    ShortenedWrapper
-} from './ShortenedAppElements';
-
+  ShortenedAppContainer,
+  ShortenedWrapper,
+} from "./ShortenedAppElements";
 
 const ShortenedApp = () => {
-        const { appError, isLoading, results, submitUrl, setResults } = useUrls();
+  const { appError, isLoading, results, submitUrl, setResults } = useUrls();
 
-        return ( <
-            ShortenedAppContainer id = 'try-app' >
-            <
-            ShortenedWrapper >
-            <
-            ShortenedFormMaster onUrlSubmit = { submitUrl }
-            /> {
-                appError ? < ErrorMessage margin = '1rem auto'
-                fontSize = '1rem' > { appError } < /ErrorMessage> : null } { isLoading ? < Loader / > : null } <
-                    LinkList results = { results }
-                setResults = { setResults }
-                /> <
-                /ShortenedWrapper> <
-                /ShortenedAppContainer>
-            )
+  return (
+    <ShortenedAppContainer id="try-app">
+      <ShortenedWrapper>
+        <ShortenedFormMaster onUrlSubmit={submitUrl} />
+        {appError ? (
+          <ErrorMessage margin="1rem auto" fontSize="1rem">
+            {appError}
+          </ErrorMessage>
+        ) : null}
+        {isLoading ? <Loader /> : null}
+        <LinkList results={results} setResults={setResults} />
+      </ShortenedWrapper>
+    </ShortenedAppContainer>
+  );
+};
 
-        }
-
-
-        export default ShortenedApp;
+export default ShortenedApp;
